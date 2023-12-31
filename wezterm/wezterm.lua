ebbab30093f3cb75ead7d687231f1bf37db47d44
Trevor Gross <tmgross@umich.edu>
2023-12-31 02:03:22 -0600

    Add wezterm config, update helix and ssh configs

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,42 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.keys = {
+  {
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = wezterm.action.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = wezterm.action.SendKey {
+      key = 'f',
+      mods = 'ALT',
+    },
+  }
+}
+
+config.window_padding = {
+  left = 4,
+  right = 4,
+  top = 4,
+  bottom = 4,
+}
+
+config.initial_cols = 120
+config.initial_rows = 30
+
+return config
